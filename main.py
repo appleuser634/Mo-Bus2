ba9549c38f454cb164f0f9ddd5baddaa75d046cc
Miyagi Musashi <miyagimusashi@MiyaginoiMac.local>
2021-05-17 23:51:11 +0900

    add network connection display

--- main.py
+++ main.py
@@ -2,20 +2,14 @@ from machine import Pin, I2C
 from ssd1306 import SSD1306_I2C
 import time
 
+import boot_display
+import network_setting
+
 i2c = I2C(scl=Pin(4), sda=Pin(5))
 oled = SSD1306_I2C(128, 64, i2c)
 
-for i in reversed(range(25,70)):
-    oled.fill(0)
-    oled.text('Mo-Bus!!', 35, i)
-    oled.show()
-
-time.sleep(1)
-
-for i in reversed(range(-10,25)):
-    oled.fill(0)
-    oled.text('Mo-Bus!!', 35, i)
-    oled.show()
-
 oled.fill(0)
 oled.show()
+
+boot_display.boot_animation()
+network_setting.connect_network('HUMAX-E938C','MmdhdGR3LThFM')
---
+++ network_setting.py
@@ -0,0 +1,37 @@
+import network
+from machine import Pin, I2C
+from ssd1306 import SSD1306_I2C
+import time
+
+i2c = I2C(scl=Pin(4), sda=Pin(5))
+oled = SSD1306_I2C(128, 64, i2c)
+
+def loop_animation(loading_charas,times):
+    for _ in range(times):
+        for l in loading_charas:
+            oled.fill(0)
+            display_string = "Connecting " + l
+            oled.text(display_string, 20, 30)
+            oled.show()
+
+def connect_network(ssid, password):
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+
+    loading_charas = ['\\','|','/','-']
+    loop_animation(loading_charas,10)
+
+    if not wlan.isconnected:
+        while not wlan.isconnected:
+            loop_animation(loading_charas,1)
+            wlan.connect(ssid, password)
+
+    oled.fill(0)
+    oled.text("ON LINE!!", 30, 30)
+    oled.show()
+
+    time.sleep(2)
+
+    oled.fill(0)
+    oled.show()
+    print('network config:', wlan.ifconfig())
