8c52ae2f2d29ea29954ea6a5dad27e02b1f5f8e9
Miyagi Musashi <miyagimusashi@MiyaginoiMac.local>
2021-05-17 14:22:14 +0900

    add test programm

---
+++ main.py
@@ -0,0 +1,21 @@
+from machine import Pin, I2C
+from ssd1306 import SSD1306_I2C
+import time
+
+i2c = I2C(scl=Pin(4), sda=Pin(5))
+oled = SSD1306_I2C(128, 64, i2c)
+
+for i in reversed(range(25,70)):
+    oled.fill(0)
+    oled.text('Mo-Bus!!', 35, i)
+    oled.show()
+
+time.sleep(1)
+
+for i in reversed(range(-10,25)):
+    oled.fill(0)
+    oled.text('Mo-Bus!!', 35, i)
+    oled.show()
+
+oled.fill(0)
+oled.show()
